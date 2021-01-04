0dd7d9e60865ba2805123141dfbc85ac43a4ec60
Sauliuszi <trasoje@gmail.com>
2021-01-04 20:25:32 +0200

    nieko gero

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+
+declare(strict_types=1); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +14,8 @@
 <body>
     <?php
     echo $_SERVER['REQUEST_URI'];
-    ?>
-    <table>
+
+    print("<table>
         <thead>
             <tr>
                 <th>Type</th>
@@ -20,18 +23,34 @@
                 <th>Action</th>
             </tr>
         </thead>
-        <tbody>
-            <?php
-            $files = scandir("../sprint1");
-            foreach ($files as $file) {
-                print("<tr> <td></td>
-        <td>$file</td> <td></td>
-    </tr>");
-            }
-            ?>
+        <tbody>");
 
-        </tbody>
-    </table>
+
+    $path = isset($_GET['path']) ? $_GET['path'] : './';
+    $files = scandir($path);
+    echo '<br>';
+    print_r(scandir('./'));
+    echo '<br>';
+    print_r(scandir('./bandymas'));
+    echo '<br>';
+
+    foreach ($files as $file) {
+        // print("<tr><td>" . is_dir($file) . "</td>");
+        print('<tr><td>' . (is_dir($file) == 1 ? "direktorija" : "failas") . '</td>');
+        print('<td><a href=$path=' . $path . '/' . $file . '>' . $file .  '</a></td>');
+        print("<td>" . (is_file($file) == 1 ? "<button>Trinti</button>" .
+            " " . "<button>Parsiuntimas</button>" : null) . "</td></tr>");
+    }
+    print ('</tbody></table>');
+    echo '<br>';
+    // print('<button id="atgal">Atgal</button><br><br>
+    //         <form action="" method="GET">
+    //             <input type="text" placeholder="Sukurti naują direktoriją">
+    //             <button>pateikti</button>
+    //         </form>');
+
+  
+?> 
 
 </body>
 
